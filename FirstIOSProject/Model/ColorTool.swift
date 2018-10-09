987587e7264bcdd9cf3162869e203749db0f2dc0
rphi8796 <jlkfop@gmail.com>
2018-10-09 12:20:56 -0600

    added model group and renamed files

---
+++ FirstIOSProject/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstIOSProject
+//
+//  Created by Phillips, Ryan on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
